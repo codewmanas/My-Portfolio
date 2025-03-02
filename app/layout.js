ababf8986594b5f2287657f8ac5a6f01f9dfce50
codewmanas <manas.kolaskar@somaiya.edu>
2025-03-02 19:45:10 +0530

    changes fixed

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-import { Outfit, Ovo, Archivo } from "next/font/google";
+import { Outfit, Ovo, } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["sans-serif"], weight: ["400", "500", "600", "700"]
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
 const ovo = Ovo({
-  subsets: ["sans-serif"], weight: ["400"]
+  subsets: ["latin"], weight: ["400"]
 });
 
 
@@ -14,6 +14,7 @@ const ovo = Ovo({
 export const metadata = {
   title: "Manas Kolaskar",
   description: "Personal Portfolio of Manas Kolaskar",
+  
 };
 
 export default function RootLayout({ children }) {
